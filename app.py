21d5c592b14a77f7a58b8329d297f78b268b3315
nadia <ubaid243@gmail.com>
2023-08-08 20:36:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,5 @@ def chat():
     user_input = request.form['user_input']
     response = get_response(user_input)
     return jsonify({'response': response})
-
-    if __name__ == '__main__':
-      app.run(debug=True) 
+if __name__ == '__main__':
+  app.run(debug=True) 

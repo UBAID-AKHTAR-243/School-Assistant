3ab32573f079c2160cc440745c151be9dcc10305
nadia <ubaid243@gmail.com>
2023-12-22 02:49:34 +0500

    on  22-12-2023 updated html,app.py
        on  22-12-2023 updated html,app.py

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from flask import Flask, render_template, request
 from chatbot import predict_class, get_response
 
+
 app = Flask(__name__)
 
 
-@app.route('/index', methods=['POST'])
+@app.route('/index', methods=['POST', "GET"])
 def index():
     return render_template("index.html")
 
 
-@app.route('/bot', methods=['POST'])
-def bot(intents_json=None):
-    user_input = request.form['user_question']
+@app.route('/submit', methods=['POST', "GET"])
+def submit():
+    user_input = request.form.get('user_question')
     intents_list = predict_class(user_input)
-    response = get_response(intents_list, intents_json) 
-    return render_template("index.html", response=response)
+    response = get_response(intents_list, "intents")
+    return render_template("index.html", user_input=user_input, response=response)
 
 
 if __name__ == '__main__':
-
+    
     app.run(debug=True)
